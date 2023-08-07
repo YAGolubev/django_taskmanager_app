357e3ed5181d2f5103c6161a6a364224803303a6
YAGolubev <yagolubev@inbox.ru>
2023-08-08 00:26:55 +0300

    Add templates for index and about page

--- project/main/urls.py
+++ project/main/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index)]
+urlpatterns = [
+    path("", views.index, name="home"),
+    path("about-us", views.about, name="about"),
+]
